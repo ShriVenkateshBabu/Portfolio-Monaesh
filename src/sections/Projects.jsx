2cc222fdf25ac92b8fd7815e134563aba049a245
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2026-02-14 14:01:13 +0530

    image chnage

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -51,19 +51,19 @@ export default function Projects() {
         title: "College Events",
         link: "https://www.instagram.com/monaesh_official?igsh=cG9lZmZiOWN5cmJp",
         bgColor: "#0d4d3d",
-        image: isMobile ? photo1 : img1, // Mobile vs desktop image
+        image: isMobile ? photo1 : photo1, // Mobile vs desktop image
       },
       {
         title: "Awards",
         link: "https://www.instagram.com/monaesh_official?igsh=cG9lZmZiOWN5cmJp",
         bgColor: "#3884d3",
-        image: isMobile ? photo2 : img2,
+        image: isMobile ? photo2 : photo2,
       },
       {
         title: "Covid 19 Awarness ",
         link: "https://www.instagram.com/monaesh_official?igsh=cG9lZmZiOWN5cmJp",
         bgColor: "#dc9317",
-        image: isMobile ? photo3 : img3,
+        image: isMobile ? photo3 : photo3,
       },
     ],
     [isMobile]
